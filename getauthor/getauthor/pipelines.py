8b268e09027445eea6434baeec6cc76b75926bb2
Junnian <347828132@qq.com>
2017-11-29 16:46:56 +0800

    wan shan le yi xia

--- getauthor/getauthor/pipelines.py
+++ getauthor/getauthor/pipelines.py
@@ -10,54 +10,52 @@ class GetauthorPipeline(object):
     def open_spider(self,spider):
         host = '127.0.0.1'
         port = 27017
-        dbname = 'authorinfo'  # 设置数据库,也就是这话怕论文的
+        dbname = 'info'  # 设置数据库
         client = pymongo.MongoClient(host=host, port=port)
         tdb = client[dbname]
-
-        self.all = tdb['aerospace']
-        # self.all = tdb['bigdata']
-        # self.all = tdb['biology']
-        # self.all = tdb['infornet']
-        # self.all = tdb['newM']
-        # self.all = tdb['QC']
-        # self.all = tdb['shipBuild']
-        
-        #存第一层合著作者
-        self.allc = tdb['aerospace_c']
-        # self.allc = tdb['bigdata_c']
-        # self.allc = tdb['biology_c']
-        # self.allc = tdb['infornet_c']
-        # self.allc = tdb['newM_c']
-        # self.allc = tdb['QC_c']
-        # self.allc = tdb['shipBuild_c']
-
-        #存第二层合著作者
-        self.allc2 = tdb['aerospace_c2']
-        # self.allc2 = tdb['bigdata_c2']
-        # self.allc2 = tdb['biology_c2']
-        # self.allc2 = tdb['infornet_c2']
-        # self.allc2 = tdb['newM_c2']
-        # self.allc2 = tdb['QC_c2']
-        # self.allc2 = tdb['shipBuild_c2']
-
-        #存第二层合著作者
-        self.allc3 = tdb['aerospace_c3']
-        # self.allc3 = tdb['bigdata_c3']
-        # self.allc3 = tdb['biology_c3']
-        # self.allc3 = tdb['infornet_c3']
-        # self.allc3 = tdb['newM_c3']
-        # self.allc3 = tdb['QC_c3']
-        # self.allc3 = tdb['shipBuild_c3']
-
-        #存无限扩展的作者
-        self.alla = tdb['aerospace_ca']
-        # self.alla = tdb['bigdata_ca']
-        # self.alla = tdb['biology_ca']
-        # self.alla = tdb['infornet_ca']
-        # self.alla = tdb['newM_ca']
-        # self.alla = tdb['QC_ca']
-        # self.alla = tdb['shipBuild_ca']
-
+        t = 
+        if t==1:
+            self.all = tdb['aerospace']
+            self.allc = tdb['aerospace_c']
+            self.allc2 = tdb['aerospace_c2']
+            self.allc3 = tdb['aerospace_c3']
+            self.allca = tdb['aerospace_ca']
+        if t==2:
+            self.all = tdb['bigdata']
+            self.allc = tdb['bigdata_c']
+            self.allc2 = tdb['bigdata_c2']
+            self.allc3 = tdb['bigdata_c3']
+            self.allca = tdb['bigdata_ca']
+        if t==3:
+            self.all = tdb['biology']
+            self.allc = tdb['biology_c']
+            self.allc2 = tdb['biology_c2']
+            self.allc3 = tdb['biology_c3']
+            self.allca = tdb['biology_ca']
+        if t==4:
+            self.all = tdb['infornet']
+            self.allc = tdb['infornet_c']
+            self.allc2 = tdb['infornet_c2']
+            self.allc3 = tdb['infornet_c3']
+            self.allca = tdb['infornet_ca']
+        if t==5:
+            self.all = tdb['newM']
+            self.allc = tdb['newM_c']
+            self.allc2 = tdb['newM_c2']
+            self.allc3 = tdb['newM_c3']
+            self.allca = tdb['newM_ca']
+        if t==6:
+            self.all = tdb['QC']
+            self.allc = tdb['QC_c']
+            self.allc2 = tdb['QC_c2']
+            self.allc3 = tdb['QC_c3']
+            self.allca = tdb['QC_ca']
+        if t==7:
+            self.all = tdb['shipBuild']
+            self.allc = tdb['shipBuild_c']
+            self.allc2 = tdb['shipBuild_c2']
+            self.allc3 = tdb['shipBuild_c3']
+            self.allca = tdb['shipBuild_ca']
 
     def process_item(self, item, spider):
         # items = dict(item)
--- readme.txt
+++ readme.txt
@@ -7,7 +7,7 @@ copinfo.py   是从copurl.txt文件里读取url,并爬取这些信息 ，并将�
 copinfo2.py   是从copurl.txt文件里读取url,并爬取这些信息 ，并将这些作者的合著直接保存再copurl2.txt文件里 
 
 
-copinfo2.py   是从copur2.txt文件里读取url,并爬取这些信息 ，并将这些作者的合著直接保存再copurl2.txt文件里 
+copinfo3.py   是从copur2.txt文件里读取url,并爬取这些信息 ，并将这些作者的合著直接保存再copurl3.txt文件里 
 
 
 otherauthor.py  爬取逻辑是，以key或new里的文件为初始关键词，每爬到一个作者，就将这个作者的所属领域添加到待爬id中,将爬到的领域写到field.txt中
